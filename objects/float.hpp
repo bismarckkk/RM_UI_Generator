b6649fa1daf5360092f19dc3ff30cef180dd809e
bismarckkk <zuoqingyu@nuaa.edu.cn>
2025-05-11 10:52:51 +0800

    fix: 修复了float组件字段错误的问题

--- objects/float.hpp
+++ objects/float.hpp
@@ -25,7 +25,33 @@ public:
 };
 
 namespace ns {
-    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Float, name, group, type, color, layer, x, y, fontSize, number);
+
+template<typename BasicJsonType, nlohmann::detail::enable_if_t<nlohmann::detail::is_basic_json<BasicJsonType>::value, int>  = 0>
+void to_json(BasicJsonType &nlohmann_json_j, const Float &nlohmann_json_t) {
+    nlohmann_json_j["name"] = nlohmann_json_t.name;
+    nlohmann_json_j["group"] = nlohmann_json_t.group;
+    nlohmann_json_j["type"] = nlohmann_json_t.type;
+    nlohmann_json_j["color"] = nlohmann_json_t.color;
+    nlohmann_json_j["layer"] = nlohmann_json_t.layer;
+    nlohmann_json_j["x"] = nlohmann_json_t.x;
+    nlohmann_json_j["y"] = nlohmann_json_t.y;
+    nlohmann_json_j["fontSize"] = nlohmann_json_t.fontSize;
+    nlohmann_json_j["float"] = nlohmann_json_t.number;
+}
+
+template<typename BasicJsonType, nlohmann::detail::enable_if_t<nlohmann::detail::is_basic_json<BasicJsonType>::value, int>  = 0>
+void from_json(const BasicJsonType &nlohmann_json_j, Float &nlohmann_json_t) {
+    nlohmann_json_j.at("name").get_to(nlohmann_json_t.name);
+    nlohmann_json_j.at("group").get_to(nlohmann_json_t.group);
+    nlohmann_json_j.at("type").get_to(nlohmann_json_t.type);
+    nlohmann_json_j.at("color").get_to(nlohmann_json_t.color);
+    nlohmann_json_j.at("layer").get_to(nlohmann_json_t.layer);
+    nlohmann_json_j.at("x").get_to(nlohmann_json_t.x);
+    nlohmann_json_j.at("y").get_to(nlohmann_json_t.y);
+    nlohmann_json_j.at("fontSize").get_to(nlohmann_json_t.fontSize);
+    nlohmann_json_j.at("float").get_to(nlohmann_json_t.number);
+}
+
 }
 
 #endif //FLOAT_HPP
